5aa5f75fb6e4baaafb4d2a5c4ecd1dbaee629655
Oshan <tharinduoshan@gmail.com>
2023-06-19 18:59:06 +0530

    fix dark mode

--- src/pages/ExperiencePage/ExperienceSection.jsx
+++ src/pages/ExperiencePage/ExperienceSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExperienceSection = ({ position, duration, description }) => {
   return (
-    <div className=' relative flex flex-col before:absolute before:left-[-35px] before:top-2 before:z-[1] before:h-4 before:w-4 before:rounded-full before:dark:bg-violet-400'>
+    <div className=' relative   flex flex-col before:absolute before:left-[-35px] before:top-2 before:z-[1] before:h-4 before:w-4 before:rounded-full before:bg-violet-400'>
       <h3 className='text-xl font-semibold tracking-wide'>{position}</h3>
       <time className='text-xs uppercase tracking-wide dark:text-gray-400'>{duration}</time>
       <p className='mt-3'>{description}</p>
--- src/pages/ExperiencePage/index.jsx
+++ src/pages/ExperiencePage/index.jsx
@@ -5,7 +5,7 @@ import { experienceData } from './data.js';
 
 const ExperiencePage = () => {
   return (
-    <section className='rounded-md dark:bg-black dark:text-gray-100 md:py-[4rem]'>
+    <section className='rounded-md bg-black text-gray-100 md:py-[4rem]'>
       <div className='container  max-w-5xl justify-items-center px-4 py-12'>
         <div className='mx-4 grid gap-4 sm:grid-cols-12'>
           <div className='relative col-span-12 space-y-6 px-4 sm:col-span-9'>
@@ -14,7 +14,7 @@ const ExperiencePage = () => {
                 <div className='-ml-4 mb-3 text-left text-xl font-semibold tracking-wide'>
                   {data.company}
                 </div>
-                <div className='relative col-span-12 space-y-12 px-4 before:absolute before:-left-3 before:bottom-0 before:top-2 before:w-0.5 before:dark:bg-gray-700 sm:col-span-8 sm:space-y-8'>
+                <div className='relative col-span-12 space-y-12 px-4 before:absolute before:-left-3 before:bottom-0 before:top-2 before:w-0.5 before:bg-gray-700 sm:col-span-8 sm:space-y-8'>
                   {data.experience.map((experience, i) => (
                     <ExperienceSection
                       description={experience.description}
