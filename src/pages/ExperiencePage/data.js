35c4d269f170c36bbe2e8f8496b1bb3bdf7a92b2
ToshaEX <tharinduoshan@gmail.com>
2025-02-08 20:30:35 +0530

    - update Experience data

--- src/pages/ExperiencePage/data.js
+++ src/pages/ExperiencePage/data.js
@@ -1,16 +1,31 @@
 export const experienceData = [
   {
-    company: 'RedBlocks Technologies (Private) Limited',
+    company: 'Ideahub (PVT) LTD',
+    experience: [
+      {
+        position: 'Software Engineer',
+        duration: 'APR 2023 - FEB 2025',
+        description: `Developed and maintained web applications using React and NestJS, creating efficient and secure systems for cash collection, payment card encryption, and product management. Designed structured architectures with focus on security, audit trails, and performance, including features like CRUD interfaces and encryption processes. Led the transition of legacy systems to modern frameworks, implemented robust security measures (CSRF, JWT, CSP), and collaborated with cross-functional teams to meet client needs, improving system performance and user experience.        `,
+      },
+      {
+        position: 'Associate Software Engineer',
+        duration: 'OCT 2023 - APR 2024',
+        description: ``,
+      },
+    ],
+  },
+  {
+    company: 'RedBlocks Technologies (PVT) LTD',
     experience: [
       {
         position: 'Associate Software Engineer',
         duration: 'MAR 2023 - MAY 2023',
-        description: 'Working as a fullstack software engineer',
+        description: `Designed and developed robust web applications using React, Next.js, NestJS, and TypeScript, implementing efficient front-end and back-end solutions. Integrated AWS services for secure document storage and version control, while ensuring role-based authentication and performance optimization. Delivered responsive, scalable, and maintainable code, providing ongoing technical support and handling deployments and automated backups.`,
       },
       {
         position: 'Intern Software Engineer',
         duration: 'SEP 2022 - MAR 2023',
-        description: 'Working as a fullstack software engineer',
+        description: ``,
       },
     ],
   },
