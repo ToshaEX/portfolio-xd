d7d6b93de4fbdb7104c3f44c5c0eb4582df211dd
ToshaEX <tharinduoshan@gmail.com>
2025-02-09 09:07:35 +0530

    title margin bottom increase

--- src/wrapper/titleWrapper.jsx
+++ src/wrapper/titleWrapper.jsx
@@ -4,7 +4,7 @@ const titleWrapper = (Components, title) =>
   function HOC() {
     return (
       <div className='mx-[1rem] flex  flex-1 flex-col py-[2rem] md:mx-[6rem] md:py-[6rem] lg:mx-[10rem]'>
-        <div className='mb-5 text-center text-5xl font-bold'>{title}</div>
+        <div className='mb-8 md:mb-10 text-center text-5xl font-bold'>{title}</div>
         <Components />
       </div>
     );
